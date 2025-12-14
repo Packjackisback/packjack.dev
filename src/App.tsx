2a82fb20bd98b84002b0f9ea9342f33e09d6475c
packjackisback <k1401308@students.katyisd.org>
2025-12-14 15:39:59 -0600

    Starting

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="page">
+			<header>
+				<h1>Jackson MacGregor</h1>
+				<p>Systems management and software dev</p>
+			</header>
+    
+			<main>
+				<h2>Who am I?</h2>
+				<p>
+					I'm Jackson MacGregor! I really enjoy clouds, fire alarms, and programming. 
+					<br/>
+					I don't usually have the commitment to finish them, but I love my little 11pm projects.
+				</p>
+			</main>
+
+			<footer>
+				<p>No copyright, do what you want! (Made by Jackson)</p>
+			</footer>
+
+    </div>
+  )
+}
+
+export default App
